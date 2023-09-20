faca4428790a91f8e6f20d04e5ed1e65a6bff09f
FarieB <faraibande@gmail.com>
2023-09-20 04:10:09 -0700

    create simple shell

--- shell_print.c
+++ shell_print.c
@@ -1,32 +1,17 @@
 #include "shell_header.h"
 
 /**
-* read_input - Function that reads input from the user
-* @input: A pointer to a pointer to char - holds the addr of the inputted str
-* @input_size: A pointer to size_t - holds the size of the inputted string
-*
-* Description: it reads the input from user.
-* Return: pointer to the inputted string
-*/
+ * shell_print - Write function to print the string to stdout (fd1)
+ * @letters: Pointer to the string we want to print
+ *
+ * Description:
+ * This function writes the given string to the standard output (stdout).
+ *
+ * @letters: Pointer to the null-terminated string to be printed.
+ */
 
-char *read_input(char **input, size_t *input_size)
+void shell_print(const char *letters)
 {
-ssize_t input_length;
-
-input_length = getline(input, input_size, stdin);
-
-if (input_length == -1)
-{
-free(*input);
-*input = NULL;
-}
-else
-{
-if (input_length > 0 && (*input)[input_length - 1] == '\n')
-{
-(*input)[input_length - 1] = '\0';
-}
+	write(1, letters, strlen(letters));
 }
 
-return (*input);
-}
